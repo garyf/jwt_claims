32c9b4b18867a71c64628f5976c9d7ad536d54de
Gary Fleshman <gfleshman@newforge-tech.com>
2015-08-26 20:47:46 -0400

    Exp#reject?

--- Gemfile
+++ Gemfile
@@ -1,3 +1,7 @@
 source 'https://rubygems.org'
 
 gemspec
+
+gem 'pry-byebug', '~> 3.1', require: false
+gem 'simplecov', '~> 0.10', require: false
+gem 'yard', '~> 0.8', require: false
---
+++ spec/jwt_claims/claim/exp_spec.rb
@@ -0,0 +1,22 @@
+require 'jwt_claims/claim/exp'
+
+module JwtClaims
+  module Claim
+    describe Exp do
+      let(:after_now) { Time.now.to_i + 1 }
+      describe '#reject?' do
+        it 'w numeric_date after now returns false' do
+          expect(Exp.reject? after_now).to be false
+        end
+        it 'w numeric_date now returns true' do
+          expect(Exp.reject? Time.now.to_i).to be true
+        end
+
+        it 'w/o numeric numeric_date returns true' do
+          expect(Exp.reject? after_now.to_s).to be true
+        end
+      end
+    end
+  end
+end
+    
